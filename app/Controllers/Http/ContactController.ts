98bfc6f92e557c07284e963aea8dbc210ff50de3
Simon <simon.duperray@reseau.eseo.fr>
2022-03-09 19:06:51 +0100

    :bookmark: send mail to gmail address

--- app/Controllers/Http/ContactController.ts
+++ app/Controllers/Http/ContactController.ts
@@ -4,7 +4,7 @@ import { ContactService } from 'App/Services/ContactService'
 export default class ContactsController {
    async store({ request, session, response }: HttpContextContract) {
       ContactService.send(request.all() as any)
-      session.flash('success', 'Your email has been sent successfully')
+      session.flash('success', 'Your email has been sent successfully !')
       return response.redirect().back()
    }
 }
