1ee99335c24569dc28b89bdda6cb60ef5fcea0cc
Simon <simon.duperray@reseau.eseo.fr>
2022-03-10 09:12:01 +0100

    :sparkles: contact form validator

--- app/Controllers/Http/ContactController.ts
+++ app/Controllers/Http/ContactController.ts
@@ -1,9 +1,10 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { ContactService } from 'App/Services/ContactService'
+import ContactMeValidator from 'App/Validators/ContactMeValidator'
 
 export default class ContactsController {
    async store({ request, session, response }: HttpContextContract) {
-      ContactService.send(request.all() as any)
+      ContactService.send(await request.validate(ContactMeValidator) as any)
       session.flash('success', 'Your email has been sent successfully !')
       return response.redirect().back()
    }
---
+++ app/Validators/ContactMeValidator.ts
@@ -0,0 +1,54 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class ContactMeValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+    name: schema.string({ trim: true }),
+    email: schema.string({ trim: true }, [
+      rules.email(),
+    ]),
+    message: schema.string({ trim: true }, [
+      rules.minLength(5),
+    ]),
+  })
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages = {
+    'name.required': 'You have to fill your name.',
+    'email.required': 'You have to fill your email.',
+    'message.required': 'You have to fill your message.',
+    'message.minLength': 'Your message must contains at least 5 characters.',
+    'email': 'Your mail does not seems to a valid one...'
+  }
+}
